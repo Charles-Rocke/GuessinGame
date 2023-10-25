0a2e092ebabfe2ba09279e58331d9f643edfd899
SpOOk <spook.devv@gmail.com>
2023-10-24 21:11:12 -0400

    Added number only text input type

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,24 +1,37 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
 
 function PrimaryButton({ children }) {
+  function pressHandler() {
+    console.log("Pressed");
+  }
   return (
-    <View>
-      <Text style={styles.textColor}>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={pressHandler}
+        android_ripple={{ color: "#000003" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  textColor: {
-    color: "#c0c0c0",
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
   },
-  inputContainer: {
-    padding: 16,
-    marginHorizontal: 24,
-    marginTop: 100,
-    backgroundColor: "#000033",
-    borderRadius: 8,
-    // elevation: 8, android box shadow name
+  buttonInnerContainer: {
+    backgroundColor: "#000",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2, // android box shadow
     shadowColor: "black", // iOS uses shadowColor, shadowOffset, shadowRadius, shadowOpacity to create a shadow
     shadowOffset: {
       width: 0,
@@ -26,6 +39,13 @@ const styles = StyleSheet.create({
     }, // determines where shadow is pushed
     shadowRadius: 6, // size of the shadow
     shadowOpacity: 0.25, // determines the strength of shadow
+  },
+  buttonText: {
+    color: "#c0c0c0",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
   },
 });
 
