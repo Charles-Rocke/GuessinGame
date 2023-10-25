a09c2ad2fd53754b5da0c8abf6e4930d23e7085f
SpOOk <spook.devv@gmail.com>
2023-10-24 20:22:33 -0400

    Added number only text input type

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,11 +1,32 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 function PrimaryButton({ children }) {
   return (
     <View>
-      <Text>{children}</Text>
+      <Text style={styles.textColor}>{children}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  textColor: {
+    color: "#c0c0c0",
+  },
+  inputContainer: {
+    padding: 16,
+    marginHorizontal: 24,
+    marginTop: 100,
+    backgroundColor: "#000033",
+    borderRadius: 8,
+    // elevation: 8, android box shadow name
+    shadowColor: "black", // iOS uses shadowColor, shadowOffset, shadowRadius, shadowOpacity to create a shadow
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    }, // determines where shadow is pushed
+    shadowRadius: 6, // size of the shadow
+    shadowOpacity: 0.25, // determines the strength of shadow
+  },
+});
 
 export default PrimaryButton;
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,7 +4,14 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      {/* maxLength prop sets total number of enterable characters */}
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -16,7 +23,7 @@ const styles = StyleSheet.create({
     padding: 16,
     marginHorizontal: 24,
     marginTop: 100,
-    backgroundColor: "#72063c",
+    backgroundColor: "#000033",
     borderRadius: 8,
     // elevation: 8, android box shadow name
     shadowColor: "black", // iOS uses shadowColor, shadowOffset, shadowRadius, shadowOpacity to create a shadow
@@ -26,6 +33,17 @@ const styles = StyleSheet.create({
     }, // determines where shadow is pushed
     shadowRadius: 6, // size of the shadow
     shadowOpacity: 0.25, // determines the strength of shadow
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#FFFCF0",
+    borderBottomWidth: 2,
+    color: "#FFFCF0",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
 
